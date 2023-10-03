27c51dc1e099ce80794628b58768407f87e1bb7e
ethan-mcconkey <ethanmcconkey@protonmail.com>
2023-10-03 12:13:57 +0200

    refactor(src): :fire: Remove navbar form home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import NavBar from "@/components/NavBar";
-
 const page: string = "home";
 
 export default function Home() {
 	return (
 		<main className={page} id={page}>
-			<NavBar currentPage={page} />
 			<h1>{page}</h1>
 		</main>
 	);
