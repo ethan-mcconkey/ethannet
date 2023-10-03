1fe5f3cfe5f25c687ae3d62f3f76ecba429a0270
Ethan McConkey <95688781+ethan-mcconkey@users.noreply.github.com>
2023-10-03 12:33:06 +0200

    Update page.tsx
    Signed-off-by: Ethan McConkey <95688781+ethan-mcconkey@users.noreply.github.com>

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,8 @@
-import NavBar from "@/components/NavBar";
-
 const page: string = "about";
 
 export default function About() {
 	return (
 		<main className={page} id={page}>
-			<NavBar currentPage={page} />
 			<h1>{page}</h1>
 		</main>
 	);
