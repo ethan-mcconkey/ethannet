dfe50d1896bff13ad4a9405ca83d768966f7f095
Ethan McConkey <95688781+ethan-mcconkey@users.noreply.github.com>
2023-10-25 17:58:52 +0000

    feat(css): :lipstick: Started footer and navbad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
     return (
         <>
             <Navbar currentPage="home" />
-            <h1>Home Page</h1>
+            <main>
+                <h1>Home Page</h1>
+            </main>
         </>
     );
 }
