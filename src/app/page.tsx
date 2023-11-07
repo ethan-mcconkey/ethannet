9f3e40a45c396ceef72f0ddc2bdc1f44d157155c
ethan-mcconkey <ethanmcconkey@protonmail.com>
2023-11-07 18:09:52 +0200

    feat(src): :sparkles: Added SSH blog post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,31 @@
+import Link from 'next/link'
+
 export default function Home() {
     return (
         <>
             <main>
-                <h1>Home Page</h1>
+                <section>
+                    <div>Portrait</div>
+                    <h1>Ethan McConkey</h1>
+                    <p>Short Description</p>
+                </section>
+                <section>
+                    <div>
+                        <h1>Skills</h1>
+                    </div>
+                    <div>
+                        <h1>Education</h1>
+                    </div>
+                </section>
+                <section>
+                    <h1>Contact Me</h1>
+                    <p>
+                        Email:{' '}
+                        <Link href="mailto:ethanmcconkey@protonmail.com">
+                            ethanmcconkey@protonmail.com
+                        </Link>
+                    </p>
+                </section>
             </main>
         </>
     )
