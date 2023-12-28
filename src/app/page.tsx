26c0a8c08f46a37112ab46d150f8f48847f7b1e5
ethan-mcconkey <ethanmcconkey@protonmail.com>
2023-12-29 00:40:10 +0200

    Remove Blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import EducationEntry from '@/components/EducationEntry'
-import SkillProgramming from '@/components/SkillProgramming'
-
 export default function Home() {
     return (
         <>
@@ -16,45 +13,6 @@ export default function Home() {
                         much as possible and to apply this to my studies and
                         future career opportunities.
                     </p>
-                </section>
-                <section>
-                    <div>
-                        <h1 className="font-bold text-center text-xl">
-                            Skills
-                        </h1>
-                        <div>
-                            <SkillProgramming
-                                language="Java"
-                                level="Advanced"
-                                learningStyle="Self Taught"
-                            />
-                            <SkillProgramming
-                                language="Python"
-                                level="Average"
-                                learningStyle="Self Taught"
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <h1 className="font-bold text-center text-xl">
-                            Education
-                        </h1>
-                        <div>
-                            <EducationEntry
-                                institute="Eduvos"
-                                instituteLink="https://www.eduvos.com/"
-                                type="Bachelor of Science in Information Technology:
-                                Security and Network Engineering"
-                                date="January 2023 - PRESENT"
-                            />
-                            <EducationEntry
-                                institute="CompTIA"
-                                instituteLink="https://www.comptia.org/"
-                                type="Network+ Certificate"
-                                date="December 2022 - PRESENT"
-                            />
-                        </div>
-                    </div>
                 </section>
             </main>
         </>
